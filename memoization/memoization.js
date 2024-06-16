7b50c2c7d02e6fbfc147e7bd5a67926641ba1ebe
Sid <sidvermas1234@gmail.com>
2024-06-16 15:52:59 +0530

    Deep Clone Polyfill

--- memoization/memoization.js
+++ memoization/memoization.js
@@ -1,30 +1,30 @@
-const factorial=(num)=>{
-  if(num===0) {
-    return 1
+const factorial = (num) => {
+  if (num === 0) {
+    return 1;
   }
-  let result=1
-  for(let i=1;i<=num;i++) {
-    result*=i
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
   }
-  return result
-}
+  return result;
+};
 
-const memoize=(callbackFn)=>{
-  const cache={}
-  return (...args)=>{
-    if(args.toString() in cache)  {
-      return cache[args.toString()]
+const memoize = (callbackFn) => {
+  const cache = {};
+  return (...args) => {
+    if (args.toString() in cache) {
+      return cache[args.toString()];
     }
-    cache[args.toString()]=callbackFn(...args)
-    return cache[args.toString()]
-  }
-}
+    cache[args.toString()] = callbackFn(...args);
+    return cache[args.toString()];
+  };
+};
 
-console.time("no_memo")
+console.time("no_memo");
 console.log(factorial(150));
-console.timeEnd("no_memo")
+console.timeEnd("no_memo");
 
-const memoizeFactorial=memoize(factorial)
-console.time("memo")
-console.log(memoizeFactorial(150))
-console.timeEnd("memo")
+const memoizeFactorial = memoize(factorial);
+console.time("memo");
+console.log(memoizeFactorial(150));
+console.timeEnd("memo");
